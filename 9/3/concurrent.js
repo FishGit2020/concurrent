3a73f28b697bc17de6192089c0134ce2beeec64e
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-04 14:21:05 +0000

    create promise with reject and resolve, use promise with .then and .catch, practiced callback

--- 9/3/concurrent.js
+++ 9/3/concurrent.js
@@ -1,27 +1,61 @@
-'use restrict'
+'use restrict';
 
-const concurrent = (callback) => {
-//    return Promise((resolve, reject) => {
-//        resolve('done');
-        // setTimeour(resolve('done'), 5000);
-//    })
-    console.log('Done')
-    return callback
-}
-
-async () => {
-    console.log('async function');
-    return concurrent().then(response => {
-        console.log(response);
-        return response;
-    })
-}
+setTimeout(() => {
+    console.log('Done one');
+}, 3000);
 
 
-() => {
-    console.log('sync function');
-    return concurrent();
-}
+setTimeout(() => {
+    console.log('Done two');
+}, 3000);
 
-concurrent();
+const concurrent = () => {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done too!');
+        }, 4000);
+    });
+
+    return promise;
+};
+
+//wrong way to use promise
+console.log(concurrent()); // pending promise
+
+// #1 way to use promise
+const getConcurrentResult = async () => {
+    console.log('At Test!');
+    let concurrentResult = await concurrent();
+    console.log(concurrentResult);
+};
+getConcurrentResult();
+
+// #2 way to use promise
+concurrent().then((concurrentResult) => {
+    console.log(concurrentResult + ' And again!');
+});
+
+const callbackFunction = (err, data) => {
+    if (err) {
+        console.log('Got error: ' + err);
+    } else {
+        console.log('This is a callback function. I got msg: ' + data);
+    }
+};
+
+const test = (err, testData, callback) => {
+    if (err) {
+        console.log('Got err: ' + err);
+        callback(err, null);
+    } else {
+        console.log('Got testData: ' + testData);
+        setTimeout(() => {
+            callback(null, testData);
+        }, 5000);
+    }
+};
+
+test(null, 'Hi', callbackFunction);
+test(null, 'Hi 2!', callbackFunction);
+test('Nope', null, callbackFunction);
 
--- 9/3/testCallback.js
+++ 9/3/testCallback.js
@@ -8,7 +8,18 @@ console.log(data.toString('utf8'));
 const data1 = fs.readFileSync('./concurrent.js', 'utf8');
 console.log(data1);
 
+fs.readFile('./concurrent.js', 'utf8', (err, dataFromCallback) => {
+    if (err) throw err;
+    console.log(dataFromCallback);
+});
+
+fs.promises.readFile('./concurrent.js', 'utf8')
+    .then((dataFromPromise) => {
+        console.log(dataFromPromise);
+    })
+    .catch((err) => {
+        throw err;
+    });
+
 console.log('Done');
-
-
 
