f3a890c73a86387aab6b32f9be57bfb35109dd27
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-13 21:35:22 +0000

    init whatsapp ui with sidebar and chat

---
+++ 9/13/whatsapp/whatsapp-mern/src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <h1>I am a sidebar</h1>
+            <div className="sidebar__header">
+
+                <div className="sidebar__headerRight">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
---
+++ 9/13/whatsapp/whatsapp-mern/src/filebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCI7tuLfl9fqNcxAIieYWzhiMNvmhFzYnQ",
+    authDomain: "whatsapp-mern-752bb.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-752bb.firebaseio.com",
+    projectId: "whatsapp-mern-752bb",
+    storageBucket: "whatsapp-mern-752bb.appspot.com",
+    messagingSenderId: "952993984392",
+    appId: "1:952993984392:web:00ebdafec5d2e15f74143d"
+  };
