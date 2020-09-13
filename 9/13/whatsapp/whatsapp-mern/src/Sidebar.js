f312565d9eae5d7de55d66fcdbc8a497fe28192e
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-13 22:07:37 +0000

    added search bar, avatar, icons with style

--- 9/13/whatsapp/whatsapp-mern/src/Sidebar.js
+++ 9/13/whatsapp/whatsapp-mern/src/Sidebar.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import './Sidebar.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import { Avatar, IconButton } from "@material-ui/core";
+
+import SearchIcon from '@material-ui/icons/Search';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>I am a sidebar</h1>
             <div className="sidebar__header">
-
+                <Avatar src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/77f9af17699807.575694d733472.jpg" />
                 <div className="sidebar__headerRight">
                     
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchIcon />
+                    <input placeholder="Search or start new chat" type="text" />
                 </div>
             </div>
         </div>
