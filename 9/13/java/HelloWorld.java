613bd36e74de58b5f43d0fc287904d535f46d878
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-13 05:45:38 +0000

    practice java hello world

---
+++ 9/13/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        for ( int index = 0; index < args.length; index++ ) {
+            System.out.println(args[index]);
+        }
+
+        System.out.println("Hello World");
+    }
+}
