e8a3b2222c902aabbc527982fed5c4a9924a5341
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-14 21:19:45 +0000

    added pusher dependecies, frontend connected to pusher

--- 9/14/whatsapp/whatsapp-mern/src/App.js
+++ 9/14/whatsapp/whatsapp-mern/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './App.css';
-import Chat from './Chat';
-import Sidebar from './Sidebar';
+import React, { useEffect } from "react";
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
 
 function App() {
+  
+  useEffect(() => {
+    const pusher = new Pusher("01beb92902dbf8e263bd", {
+      cluster: "us2"
+    });
+
+    const channel = pusher.subscribe("message");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+    return () => {}
+  }, [])
+
   return (
     <div className="app">
       <div className="app__body">
