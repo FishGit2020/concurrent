f2c54fff80a4366772512470f6e8062a7de226c0
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-14 05:59:11 +0000

    move to date 14

---
+++ 9/14/whatsapp/whatsapp-mern/src/Chat.js
@@ -0,0 +1,65 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import React from 'react'
+import './Chat.css'
+import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons";
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+
+function Chat() {
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Youpeng</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat__message chat__reciever">
+                    <span className="chat__name">Youpeng</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <IconButton>
+                    <InsertEmoticonIcon />
+                </IconButton>
+                <form>
+                    <input placeholder="Type a message" type="text" />
+                    <button type="submit">
+                        Send a message
+                    </button>
+                </form>
+                <IconButton>
+                    <MicIcon />
+                </IconButton>
+            </div>
+
+        </div>
+    )
+}
+
+export default Chat
---
+++ 9/14/whatsapp/whatsapp-mern/src/Sidebar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Sidebar.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import { Avatar, IconButton } from "@material-ui/core";
+
+import SearchIcon from '@material-ui/icons/Search';
+
+import SidebarChat from './SidebarChat'
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div className="sidebar__header">
+                <Avatar src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/77f9af17699807.575694d733472.jpg" />
+                <div className="sidebar__headerRight">
+                    
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchIcon />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
