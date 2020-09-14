3fbac905dccedf5e023710291accda3d85abc5cf
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-14 04:54:36 +0000

    added chat header info and icons

--- 9/13/whatsapp/whatsapp-mern/src/Chat.js
+++ 9/13/whatsapp/whatsapp-mern/src/Chat.js
@@ -1,10 +1,36 @@
+import { Avatar, IconButton } from '@material-ui/core'
 import React from 'react'
 import './Chat.css'
+import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons";
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am the chat</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p>This is a message</p>
+            </div>
+
         </div>
     )
 }
