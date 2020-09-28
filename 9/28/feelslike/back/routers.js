067d10733ad435dcaee65b7d8b90fc1be69324e6
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-28 18:07:57 +0000

    added weather api for feelslike call

--- 9/28/feelslike/back/routers.js
+++ 9/28/feelslike/back/routers.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import got from 'got';
 
 const routes = express.Router();
+const openWeatherApiKey = '3442bc148b46b294a5ce5abf9240896d';
 
 /**
  * @swagger
@@ -14,5 +16,45 @@ const routes = express.Router();
 routes.get('/', (req, res) => {
     res.send("Hi");
 });
+
+
+/**
+ * @swagger
+ * /api/feelslike/{cityName}:
+ *   get:
+ *     description: feels like temperature
+ *     parameters:
+ *       - name: cityName
+ *         in: path
+ *         required: true
+ *     responses:
+ *       '200':
+ *         description: a successful response
+ */
+routes.get('/feelslike/:cityName?', (req, res) => {
+    if (req.params.cityName === undefined || req.params.cityName === '') {
+        res.status(500).send("missing city name");
+        console.error( "feels like call missing city name");
+        return;
+    }
+    const cityName = req.params.cityName;
+    const url = `http://api.openweathermap.org/data/2.5/weather` + 
+        `?q=${cityName}&appid=${openWeatherApiKey}&units=imperial`;
+    
+    got(url).then((data) => {
+        return JSON.parse(data.body);
+    }).then((weather) => {
+        console.log("found weather at " + cityName);
+        res.send(weather.main);
+    }).catch((error) => {
+        console.log(error.stack);
+        res.status(500).send(error.message);
+    })
+    
+});
+
+
+
+
 
 export default routes;
