e9b30edf61feafb6c34e58a93a0ed0996fd9163a
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-29 17:54:24 +0000

    init swagger

--- 9/29/feelslike/back/routers.js
+++ 9/29/feelslike/back/routers.js
@@ -2,6 +2,16 @@ import express from 'express';
 
 const routers = express.Router();
 
+/**
+ * @swagger
+ * /api:
+ *   get:
+ *     summary: get hello
+ *     description: get hello
+ *     responses:
+ *       '200':
+ *         description: a successful response
+ */
 routers.get('/', (req, res) => {
     res.send("Hello");
 });
--- 9/29/feelslike/back/server.js
+++ 9/29/feelslike/back/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import routers from './routers.js';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from './swaggerDoc.js';
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use('/api', routers);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(port, () => {
     console.log("server runs at: " + port);
