758f9bdd345eb8f757e394717849ea29fae919ea
youpeng.huang <youpeng.huang@halliburton.com>
2020-09-29 18:01:37 +0000

    connect to mongodb

--- 9/29/feelslike/back/server.js
+++ 9/29/feelslike/back/server.js
@@ -4,12 +4,28 @@ import routers from './routers.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDoc from './swaggerDoc.js';
 
+import mongoose from 'mongoose';
+
 const app = express();
 const port = process.env.PORT || 8080;
+const dbPw = 'dbUserPassword';
+const dbName = 'feelslikedb';
+const dbUrl = `mongodb+srv://dbUser:${dbPw}@cluster0.evesq.mongodb.net/` +
+    `${dbName}?retryWrites=true&w=majority`;
 
 app.use('/api', routers);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-app.listen(port, () => {
-    console.log("server runs at: " + port);
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("database connected");
+
+    app.listen(port, () => {
+        console.log("server runs at: " + port);
+    });
+}).catch((error) => {
+    console.error(error.stack);
 });
+
