f4ebf03e74caab7eb9ff44c29be5951ea15bfc9c
youpeng.huang <youpeng.huang@halliburton.com>
2020-10-13 23:55:43 +0000

    kafka testing

---
+++ 10/13/kafka/server.js
@@ -0,0 +1,58 @@
+
+import express from 'express';
+import kafka from 'kafka-node';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+const client = new kafka.KafkaClient({kafkaHost: 'kafka:9092'});
+const producer = new kafka.Producer(client);
+
+const consumer = new kafka.HighLevelConsumer(client, topics);
+
+const topics = [
+    { topic: 'test', partition: 1 }
+]
+
+client.on('ready', () => {
+    console.log("Client is ready.");
+});
+
+client.on('error', (err) => {
+    console.log("Client error: " + err.stack);
+});
+
+producer.on('ready', () => {
+    console.log("Producer is ready.");
+});
+
+producer.on('error', (err) => {
+    console.log("Producer error: " + err.stack);
+});
+
+app.get('/', (req, res) => {
+    console.log("Called Hi");
+    res.send("Hi");
+});
+
+app.get('/kafka/send', (req, res) => {
+    console.log("At kafka send.");
+
+    const msg = [
+        { topic: 'topic1', message: 'hi', timestamp: new Date() },
+        { topic: 'topic2', message: 'hello', timestamp: new Date() },
+    ];
+
+    producer.send(msg, (err, data) => {
+        if (err) {
+            console.error(err.stack);
+            res.send(err.message);
+        }
+
+        res.send(data);
+    });
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+})
